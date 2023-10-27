5da5a6ccafe5ddb630cc6ec91f58c3639fad5e5d
FantaCovid-19 <contact@mrfantasma.com>
2023-10-27 18:53:07 -0300

    feat: ✨ added checks and path handler to the app
    Removed unused variables by adding the path handler, plus the error handler and renaming some classes

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,23 +1,27 @@
-import type { Application, Request, Response } from 'express';
+import type { Application } from 'express';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
 import { configs, IConfig } from '@config';
-import { logger } from '@utils/logger';
+import { logger, stream } from '@utils/logger.util';
+import IndexRouter from '@routes/index.route';
+import errorMiddleware from '@middlewares/error.middleware';
 
 export default class App {
   private app: Application;
   private port: string | number;
   private config: IConfig = configs;
+  private indexRouter = new IndexRouter();
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 3000;
 
-    this.configure();
-    this.init();
+    this.initializeConfigure();
+    this.initializeRoutes();
+    this.initializeErrorHandling();
   }
 
   public start(): void {
@@ -32,18 +36,16 @@ export default class App {
     });
   }
 
-  private init(): void {
-    this.app.get('/', (req: Request, res: Response) => {
-      res.status(200).json({ message: 'Hello World!' });
-    });
-  }
-
-  private async configure(): Promise<void> {
-    this.app.use(cors());
+  private async initializeConfigure(): Promise<void> {
+    this.app.use(morgan('dev', { stream }));
+    this.app.use(cors()); // { origin: '*', credentials: true}
     this.app.use(helmet());
-    this.app.use(morgan('dev'));
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  private initializeRoutes(): void {
+    this.app.use('/api/v1', this.indexRouter.getRouter());
   }
 
   private normalizePort(port: number | string): number {
@@ -52,5 +54,9 @@ export default class App {
     }
 
     return Number(port);
+  }
+
+  private initializeErrorHandling(): void {
+    this.app.use(errorMiddleware);
   }
 }
