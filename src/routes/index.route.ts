8db6d9cef57f33ed9942175e203338d6f121545e
FantaCovid-19 <contact@mrfantasma.com>
2023-10-27 18:45:18 -0300

    feat: ✨added path handler

---
+++ src/routes/index.route.ts
@@ -0,0 +1,30 @@
+import { getAllFiles } from '@utils/getFiles.util';
+import { Router } from 'express';
+
+export default class IndexRouter {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+
+    this.loadRoutes();
+  }
+
+  private async loadRoutes() {
+    const pathRoutes: string[] = getAllFiles('src/routes');
+
+    for (const path of pathRoutes) {
+      const routeName = path.split('src\\routes\\')[1];
+      if (routeName === 'index.route.ts') continue;
+
+      const routeImport = (await import(`@routes/${routeName}`)).default;
+      const routeClass = new routeImport();
+
+      this.router.use(routeClass.path, routeClass.getRouter());
+    }
+  }
+
+  public getRouter(): Router {
+    return this.router;
+  }
+}
