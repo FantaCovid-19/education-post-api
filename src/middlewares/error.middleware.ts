891f9fa18feba110b9698ab224af08582e0e08b6
FantaCovid-19 <contact@mrfantasma.com>
2023-10-28 20:42:40 -0300

    refactor: 🎨 Rewrote the middleware bug to convert it to poo

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,18 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 
 import { HttpException } from '@helpers/httpException.helper';
-import { logger } from '@/utils/logger.util';
+import { logger } from '@utils/logger.util';
 
-const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
-  try {
-    const status: number = error.status || 500;
-    const message: string = error.message || 'Something went wrong';
+export default class ErrorMiddleware {
+  public httpExceptionError = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
+    try {
+      const status: number = error.status || 500;
+      const message: string = error.message || 'Something went wrong';
 
-    logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
-    res.status(status).json({ message });
-  } catch (error) {
-    next(error);
-  }
-};
-
-export default errorMiddleware;
+      logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
+      res.status(status).json({ message });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
