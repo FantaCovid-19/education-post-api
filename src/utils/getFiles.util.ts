b6dece1ffcbac1903c3cd192f3726c1183455478
FantaCovid-19 <contact@mrfantasma.com>
2023-11-01 22:54:26 -0300

    feat!: ✨ The getAllFiles was reconverted from scratch

--- src/utils/getFiles.util.ts
+++ src/utils/getFiles.util.ts
@@ -1,17 +1,27 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
+import { loggerUtils } from './logger.util';
 
-export function getAllFiles(dirPath: string, files: string[] = []): string[] {
-  const filesInDirectory = fs.readdirSync(dirPath, { withFileTypes: true });
+export async function getAllFiles(relativeDirPath: string, extensions: string[] = ['.js', '.ts']): Promise<string[]> {
+  const srcDirPath = path.join(__dirname, '..');
+  const dirPath = path.join(srcDirPath, relativeDirPath);
+  const files = [];
 
-  for (const file of filesInDirectory) {
-    const fullPath = path.join(dirPath, file.name);
+  try {
+    const filesInDirectory = await fs.readdir(dirPath, { withFileTypes: true });
 
-    if (file.isDirectory()) {
-      getAllFiles(fullPath, files);
-    } else if (file.isFile() && fullPath.endsWith('.ts')) {
-      files.push(fullPath);
+    for (const file of filesInDirectory) {
+      const fullPath = path.join(dirPath, file.name);
+
+      if (file.isDirectory()) {
+        const filesInSubDir = await getAllFiles(fullPath, extensions);
+        files.push(...filesInSubDir);
+      } else if (extensions.some((ext) => fullPath.endsWith(ext))) {
+        files.push(fullPath);
+      }
     }
+  } catch (err) {
+    loggerUtils.error(`Error while reading files in directory ${dirPath}: ${err}`);
   }
 
   return files;
